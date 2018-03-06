c58ad7c50cec75dfe15295a21dbdad54502a36df
Paul Obhi <paul.obhi@rci.rogers.ca>
2018-03-06 12:15:22 -0500

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, LOCALE_ID, Inject } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  languages = [
+    { code: 'en', label: 'English' },
+    { code: 'fr', label: 'Français' }
+  ];
+  constructor(@Inject(LOCALE_ID) protected localeId: string) { }
+}
